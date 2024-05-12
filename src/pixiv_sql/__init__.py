087c43822dc9d57308802c6e931e75779efdc583
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-05-12 14:13:20 +0900

    feat: Add logger.

--- src/pixiv_sql/__init__.py
+++ src/pixiv_sql/__init__.py
@@ -1,3 +1,18 @@
+from pixiv_sql.lib.logger import init_logger
+
+
 def main() -> int:
     print("Hello from pixiv-sql!")
+
+    app = PixivSQL()
+
     return 0
+
+
+class PixivSQL:
+    def __init__(self):
+        """
+        The constructor for the PixivSQL class.
+        """
+
+        self.logger = init_logger()
