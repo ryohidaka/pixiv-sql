d927a1ef59f856813b9ed4a85bb22f1589772cb6
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-07-11 18:15:19 +0900

    fix: Remove unused module.

--- src/pixiv_sql/lib/__init__.py
+++ src/pixiv_sql/lib/__init__.py
@@ -1,6 +1,6 @@
 from .logger import init_logger
 from .pixiv import generate_unique_index, get_filename, get_restrict, is_ignore_file
-from .sql import create_tables, get_engine, read_query, upsert
+from .sql import create_tables, get_engine, upsert
 
 
 __all__ = [
@@ -11,6 +11,5 @@ __all__ = [
     get_restrict,
     init_logger,
     is_ignore_file,
-    read_query,
     upsert,
 ]
